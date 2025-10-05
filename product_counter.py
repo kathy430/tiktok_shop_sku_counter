cfd4d458b2558c7dcc392de8ac6b8477ebd9dd0e
kathy <itscathywithak@gmail.com>
2025-10-05 15:12:23 -0700

    ignore all csv and started on product counter

--- product_counter.py
+++ product_counter.py
@@ -1,2 +1,22 @@
 import pandas as pd
+import os
 
+# csv files
+tiktok_file = "Shipped order-2025-10-02-22_07.csv"
+product_file = "productListUpdated.csv"
+monthly_report_file = "monthlyBarcodeReport.csv"
+
+# open csv files
+tiktok_data = pd.read_csv(tiktok_file)
+product_db = pd.read_csv(product_file)
+
+if not os.path.exists(monthly_report_file):
+    print("Monthly Barcode Report not found!, Creating file...")
+    empty_db = pd.DataFrame(columns = ["Product SKU", "Quantity"])
+    empty_db.to_csv(monthly_report_file, index = False)
+
+monthly_barcodes = pd.read_csv(monthly_report_file)
+
+
+# read TikTok order data and quantify the amount of each barcode used
+print(tiktok_data.columns)
