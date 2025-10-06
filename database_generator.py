cb530187539c73623d8461cc60bea4a620c7e15e
kathy <itscathywithak@gmail.com>
2025-10-05 20:01:37 -0700

    added more product sku columns for more combo items

--- database_generator.py
+++ database_generator.py
@@ -9,12 +9,23 @@ product_file = "productList.csv"
 if not os.path.exists(product_file):
     print("Product list not found. Creating a new one...")
     empty_db = pd.DataFrame(columns = ["Seller SKU", "Variation", "Product SKU", 
-                                       "Product SKU 2", "Quantity Used"])
+                                       "Product SKU 2", "Product SKU 3", "Product SKU 4",
+                                       "Product SKU 5", "Product SKU 6", "Product SKU 7",
+                                       "Quantity Used"])
     empty_db.to_csv(product_file, index = False)
+
+# check if product list csv file has all columns
+product_data = pd.read_csv(product_file)
+needed_cols = ["Seller SKU", "Variation", "Product SKU", 
+               "Product SKU 2", "Product SKU 3", "Product SKU 4",
+               "Product SKU 5", "Product SKU 6", "Product SKU 7",
+               "Quantity Used"]
+
+missing = [col for col in needed_cols if col not in product_data.columns]
 
 # open csv files
 tiktok_data = pd.read_csv(tiktok_file)
-product_data = pd.read_csv(product_file)
+
 
 # creating composite keys
 tiktok_data["key"] = tiktok_data["Seller SKU"].astype(str).str.strip() + "_" + tiktok_data["Variation"].astype(str).str.strip()
@@ -34,10 +45,14 @@ else:
     print(new_items[["Seller SKU", "Variation"]].to_string(index = False))
 
     # add to product list
-    new_items["Product SKU"] = "N/A"
-    new_items["Product SKU 2"] = "N/A"
-    new_items["Quantity Used"] = "N/A"
-    new_items = new_items[["Seller SKU", "Variation", "Product SKU", "Product SKU 2", "Quantity Used"]]
+    new_items[["Product SKU", "Product SKU 2", "Product SKU 3", 
+               "Product SKU 4", "Product SKU 5", "Product SKU 6", 
+               "Product SKU 7"]] = "N/A"
+    
+    new_items = new_items[["Seller SKU", "Variation", "Product SKU", 
+                           "Product SKU 2","Product SKU 3", 
+                           "Product SKU 4", "Product SKU 5", "Product SKU 6", 
+                           "Product SKU 7", "Quantity Used"]]
 
     updated_product_data = pd.concat([product_data.drop(columns = ["key"]), new_items], ignore_index = True)
     updated_product_data.to_csv(product_file, index = False)
