b1f9cc77a1d0324f9ea8088218492cbb53eac213
kathy <itscathywithak@gmail.com>
2025-10-07 16:47:46 -0700

    fixed writing to csv problem

--- database_generator.py
+++ database_generator.py
@@ -2,24 +2,23 @@ import pandas as pd
 import os
 
 # enter csv file name
-tiktok_file = "Shipped order-2025-10-02-22_07.csv"
+tiktok_file = "Shipped order-2025-10-06-15_15.csv"
 product_file = "productList.csv"
 
 # if product list csv doesnt exist, create it
 if not os.path.exists(product_file):
     print("Product list not found. Creating a new one...")
-    empty_db = pd.DataFrame(columns = ["Seller SKU", "Variation", "Quantity Used", "Product SKU", 
+    product_data = pd.DataFrame(columns = ["Seller SKU", "Variation", "Quantity Used", "Product SKU", 
                                        "Product SKU 2", "Product SKU 3", "Product SKU 4",
                                        "Product SKU 5", "Product SKU 6", "Product SKU 7"])
-    empty_db.to_csv(product_file, index = False)
+else:
+    # check if product list csv file has all columns
+    product_data = pd.read_csv(product_file)
+    needed_cols = ["Seller SKU", "Variation", "Quantity Used", "Product SKU", 
+                   "Product SKU 2", "Product SKU 3", "Product SKU 4",
+                   "Product SKU 5", "Product SKU 6", "Product SKU 7"]
 
-# check if product list csv file has all columns
-product_data = pd.read_csv(product_file)
-needed_cols = ["Seller SKU", "Variation", "Quantity Used", "Product SKU", 
-               "Product SKU 2", "Product SKU 3", "Product SKU 4",
-               "Product SKU 5", "Product SKU 6", "Product SKU 7"]
-
-missing = [col for col in needed_cols if col not in product_data.columns]
+    missing = [col for col in needed_cols if col not in product_data.columns]
 
 # open csv files
 tiktok_data = pd.read_csv(tiktok_file)
@@ -43,7 +42,7 @@ else:
     print(new_items[["Seller SKU", "Variation"]].to_string(index = False))
 
     # add to product list
-    new_items[["Product SKU", "Product SKU 2", "Product SKU 3", 
+    new_items[["Quantity Used", "Product SKU", "Product SKU 2", "Product SKU 3", 
                "Product SKU 4", "Product SKU 5", "Product SKU 6", 
                "Product SKU 7"]] = "N/A"
     
